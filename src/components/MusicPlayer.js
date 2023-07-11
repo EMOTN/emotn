92ac8877db36c6f480bd0711df52fb2eaaa82a92
Janice Fisher <janicefisher202@gmail.com>
2023-07-11 15:54:47 -0500

    adding more music

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -55,10 +55,11 @@ const MusicPlayer = () => {
                     <div>
                         {genre === 'classical' && (
                             <div>
-                            <button onClick={() => handleSubgenreClick('subgenre1')}>S</button>
-                            <button onClick={() => handleSubgenreClick('subgenre2')}>Sub-Genre 2</button>
-                            <button onClick={() => handleSubgenreClick('subgenre1')}>Sub-Genre 1</button>
-                            <button onClick={() => handleSubgenreClick('subgenre2')}>Sub-Genre 2</button>
+                            <button onClick={() => handleSubgenreClick('baroque')}>Baroque</button>
+                            <button onClick={() => handleSubgenreClick('romantic')}>Romantic</button>
+                            <button onClick={() => handleSubgenreClick('renaissance')}>Renaissance</button>
+                            <button onClick={() => handleSubgenreClick('chamber')}>Chamber</button>
+                            <button onClick={() => handleSubgenreClick('modern')}>Modern</button>
                             </div>
                         )}
                         {genre === 'nature' && (
@@ -67,30 +68,35 @@ const MusicPlayer = () => {
                             <button onClick={() => handleSubgenreClick('thunderstorm')}>Thunderstorm</button>
                             <button onClick={() => handleSubgenreClick('birds')}>Bird Songs</button>
                             <button onClick={() => handleSubgenreClick('campfire')}>Campfire</button>
+                            <button onClick={() => handleSubgenreClick('desert night')}>Desert Night</button>
                             </div>
                         )}
                         {genre === 'instrumental' && (
                             <div>
-                            <button onClick={() => handleSubgenreClick('subgenre1')}>Sub-Genre 1</button>
-                            <button onClick={() => handleSubgenreClick('subgenre2')}>Sub-Genre 2</button>
-                            <button onClick={() => handleSubgenreClick('subgenre1')}>Sub-Genre 1</button>
-                            <button onClick={() => handleSubgenreClick('subgenre2')}>Sub-Genre 2</button>
+                            <button onClick={() => handleSubgenreClick('jazz')}>Jazz</button>
+                            <button onClick={() => handleSubgenreClick('folk guitar')}>Folk Guitar</button>
+                            <button onClick={() => handleSubgenreClick('lounge')}>Lounge</button>
+                            <button onClick={() => handleSubgenreClick('reggae')}>Reggae</button>
+                            <button onClick={() => handleSubgenreClick('soft electronic')}>Soft Electronic</button>
+                            <button onClick={() => handleSubgenreClick('upbeat electronic')}>Upbeat Electronic</button>
                             </div>
                         )}
                         {genre === 'ambient/ethereal' && (
                             <div>
-                            <button onClick={() => handleSubgenreClick('subgenre1')}>Sub-Genre 1</button>
-                            <button onClick={() => handleSubgenreClick('subgenre2')}>Sub-Genre 2</button>
-                            <button onClick={() => handleSubgenreClick('subgenre1')}>Sub-Genre 1</button>
-                            <button onClick={() => handleSubgenreClick('subgenre2')}>Sub-Genre 2</button>
+                            <button onClick={() => handleSubgenreClick('new age')}>New Age</button>
+                            <button onClick={() => handleSubgenreClick('zen')}>Zen</button>
+                            <button onClick={() => handleSubgenreClick('binaural')}>Binaural Vibrations</button>
+                            <button onClick={() => handleSubgenreClick('minimalist')}>Minimalist</button>
+                            <button onClick={() => handleSubgenreClick('drone')}>Drone</button>
                             </div>
                         )}
                         {genre === 'background noise' && (
                             <div>
-                            <button onClick={() => handleSubgenreClick('subgenre1')}>Sub-Genre 1</button>
-                            <button onClick={() => handleSubgenreClick('subgenre2')}>Sub-Genre 2</button>
-                            <button onClick={() => handleSubgenreClick('subgenre1')}>Sub-Genre 1</button>
-                            <button onClick={() => handleSubgenreClick('subgenre2')}>Sub-Genre 2</button>
+                            <button onClick={() => handleSubgenreClick('airplane')}>Airplane</button>
+                            <button onClick={() => handleSubgenreClick('train cabin')}>Train Cabin</button>
+                            <button onClick={() => handleSubgenreClick('wind chimes')}>Wind Chimes</button>
+                            <button onClick={() => handleSubgenreClick('white noise')}>White Noise</button>
+                            <button onClick={() => handleSubgenreClick('brown noise')}>Brown Noise</button>
                             </div>
                         )}
                     </div>
@@ -99,7 +105,8 @@ const MusicPlayer = () => {
                     <div>
                        <AudioPlayer
                             autoPlay
-                            loop // Set the loop prop to true to play the audio on a loop
+                            loop
+                            volume 
                             src={audioFile}
                         />
                     </div>
