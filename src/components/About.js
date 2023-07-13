dcaa68c1f99fec1329cd9f1fe39420c5d7756a00
Janice Fisher <janicefisher202@gmail.com>
2023-07-13 15:30:56 -0500

    started
    '

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 
 function About() {
   return (
     <div>
-      <h1>About EMOTN</h1>
+      <h1>Meet Team EMOTN</h1>
+          <h2>Nataliia Kazik</h2>
+          <h5>Full Stack Developer</h5>
+          <img src='src/components/Our Photos/A0DB4601-68A6-4160-9B31-F2B5323CA05E_1_105_c.jpg' alt="Nataliia Kazik"/>
       <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
+      Nataliia Kazik is a software developer with a knack for mindful living. Born in Ukraine, she’s an avid traveler and a cat lover. She’s passionate about modern technologies, enjoys using them to craft delightful user experiences and is currently looking to give her career as a software engineer a strong start.
       </p>
+      <FontAwesomeIcon icon="fa-brands fa-linkedin" style={{color: "#0072b1",}} />
     </div>
   );
 }
