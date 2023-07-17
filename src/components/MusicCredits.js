50b6e4e240b8797318059a10e665bb669d26943b
Janice Fisher <janicefisher202@gmail.com>
2023-07-17 09:29:57 -0500

    music credits pt1

---
+++ src/components/MusicCredits.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const MusicCredits = () => {
+  return (
+    <div>
+        <br />
+      <h1>EMOTN Audio Credits</h1>
+      <hr></hr>
+      <h3 style={{ textDecoration: 'underline' }}>Classical</h3>
+        <ul >
+        <h4>Baroque</h4>
+            <ul>
+            <li>Concerto Brandeburghese no. 5 - I Movimento: Allegro</li>
+                <li>J.S. Bach, Combattimento Consort Amsterdam, Jan Willem de Vriend – Brandenburg Concertos (2005)</li>
+                <li><a href="https://www.youtube.com/watch?v=Cij9ftIKrIM">https://www.youtube.com/watch?v=Cij9ftIKrIM</a></li>
+            </ul>
+            <h4>Renaissance</h4>
+            <ul>
+            <li>Spem in alium (Arr. for oboe) : Spem in Alium - Thomas Tallis</li>
+                <li>Huelgas-Ensemble · Paul Van Nevel – 40 Voices (2007)</li>
+                <li><a href="https://www.youtube.com/watch?v=8uARuKnO44A">https://www.youtube.com/watch?v=8uARuKnO44A</a></li>
+            </ul>
+            <h4>Chamber</h4>
+            <ul>
+            <li>Serenade No. 10 in B-Flat Major, K. 361, "Gran Partita": IV. Adagio - Woflgang Amadeus Mozart</li>
+                <li>Mozart, German Wind Soloists – Serenade No. 10 "Gran Partita" (1987)</li>
+                <li><a href="https://www.youtube.com/watch?v=7Av_FJJwfTQ">https://www.youtube.com/watch?v=7Av_FJJwfTQ</a></li>
+            </ul>
+            <h4>Modern</h4>
+            <ul>
+            <li>In the Upper Room: Dance II - Michael Riesman · Philip Glass · Philip Glass Ensemble</li>
+                <li>Philip Glass - DancePieces (1987)</li>
+                <li>Copyright © – CBS Inc.</li>
+                <li><a href="https://www.youtube.com/watch?v=bsS0m0tObb0&list=OLAK5uy_kddcjEp0O4ljkyawvAsagnm_WVRFvvDiE&index=3">https://www.youtube.com/watch?v=bsS0m0tObb0&list=OLAK5uy_kddcjEp0O4ljkyawvAsagnm_WVRFvvDiE&index=3</a></li>
+            </ul>
+        </ul>
+    </div>
+  );
+};
+
+export default MusicCredits;
